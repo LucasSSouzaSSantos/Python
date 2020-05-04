e05535a8795f947f446215c23482085236ab44cf
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-04 19:04:51 +0000

    Delete Ex013.py

--- Ex013.py
+++
@@ -1,8 +0,0 @@
-"""
-EXERCÍCIO 013: Reajuste Salarial
-Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-"""
-salario = float(input("Informe o Salário: "))
-
-print(f"O reajuste de {salario * 0.15} e seu novo salário é: {round(1.15 * salario, 4)}")
-
