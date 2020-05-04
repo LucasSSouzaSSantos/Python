10475fcc63a0b5f859aedd5276f1b5e17a41fd97
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-04 19:00:46 +0000

    Delete Ex003.py

--- Ex003.py
+++
@@ -1,9 +0,0 @@
-"""
-EXERCÍCIO 003: Somando Dois Números
-Crie um programa que leia dois números e mostre a soma entre eles.
-"""
-
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-
-print(f"{num1} + {num2} = {num1 + num2}")
