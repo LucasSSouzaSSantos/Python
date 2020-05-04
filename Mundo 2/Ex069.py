5f37724a7941e12efedfff8d4e245289e6aa63c6
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-04 19:22:25 +0000

    Update Ex069.py

--- Mundo 2/Ex069.py
+++ Mundo 2/Ex069.py
@@ -6,22 +6,27 @@ A) Quantas pessoas têm mais de 18 anos.
 B) Quantos homens foram cadastrados.
 C) Quantas mulheres têm menos de 20 anos.
 """
-mais18 = 0
+pessoasMaisDe18 = 0
 homensCadastrado = 0
 mulheresMenos20 = 0
+cont = 0
 while True:
+    cont += 1
     idade = int(input("Idade: "))
-    sexo = str(input("Sexo: "))
+    sexo = str(input("Sexo: ")).upper().strip()
     if idade > 18:
-        mais18 += 1
+        pessoasMaisDe18 += 1
     if sexo[0] == 'M':
         homensCadastrado += 1
-    if sexo[0] == 'M' and idade < 20:
+    if sexo[0] == 'F' and idade < 20:
         mulheresMenos20 += 1
     continuar = str(input("Continuar[Ss/Nn]: ")).upper().strip()
     while continuar not in ('S', 'N'):
         continuar = str(input("Continuar[Ss/Nn]: ")).upper().strip()
     if continuar == 'N':
         break
+print(f"Pessoas cadastradas: {cont}")
+print(f"Mas de 18: {pessoasMaisDe18}")
+print(f"Homens cadastrado: {homensCadastrado}")
+print(f"Mulheres com menos de 20 anos: {mulheresMenos20}")
 
-print(f"Mas de 18: {mais18}")
