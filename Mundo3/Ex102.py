c6061680241e954628e68d85f151905fac789c1f
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-12 22:38:24 +0000

    Update Ex102.py

--- Mundo3/Ex102.py
+++ Mundo3/Ex102.py
@@ -4,15 +4,19 @@ Crie um programa que tenha uma função fatorial() que receba dois parâmetros: 
 o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será
 mostrado ou não na tela o processo de cálculo do fatorial.
 """
-
-
-def fatorial(n, show):
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
     cont = n
     fat = 1
     desenvolvimento = str(cont)
     while cont > 0:
         if cont - 1 > 0:
-            desenvolvimento = desenvolvimento + 'x' + str(cont -1)
+            desenvolvimento = desenvolvimento + 'x' + str(cont - 1)
         fat *= cont
         cont -= 1
     if show:
@@ -22,4 +26,4 @@ def fatorial(n, show):
 
 
 num = int(input("Fatorial: "))
-print(fatorial(num, False))
+print(fatorial(num, True))
