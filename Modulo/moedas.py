9c807ec6fd9ab83c02df30650386107b658d09ab
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-13 21:53:46 +0000

    Create moedas.py

---
+++ Modulo/moedas.py
@@ -0,0 +1,15 @@
+def aumentar(n, p):
+    return n + (n * p) / 100
+
+
+def diminuir(n, p):
+    return n - (n * p) / 100
+
+
+def dobro(p):
+    return 2 * p
+
+
+def metade(p):
+    return p / 2
+    
