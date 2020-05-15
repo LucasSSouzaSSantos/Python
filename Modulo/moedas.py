1cdc2d529f1c3066627f7eb3c4f6752bde8cdf21
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-15 18:22:47 -0300

    Add files via upload

--- Modulo/moedas.py
+++ Modulo/moedas.py
@@ -1,18 +1,46 @@
-def aumentar(preco=0, taxa=0):
-    return preco + (preco * taxa) / 100
+def aumentar(preco=0, taxa=0, f=False):
+    res = preco + (preco * taxa) / 100
+    if f:
+        return moeda(res)
+    else:
+        return f"{res:.2f}"
 
 
-def diminuir(preco=0, taxa=0):
-    return preco - (preco * taxa) / 100
+def diminuir(preco=0, taxa=0, f=False):
+    res = preco * (1 - taxa / 100)
+    if f:
+        return moeda(res)
+    else:
+        return f"{res:.2f}"
 
 
-def dobro(preco=0):
-    return 2 * preco
+def dobro(preco=0, f=False):
+    res = 2 * preco
+    if f:
+        return moeda(res)
+    else:
+        return f"{res:.2f}"
 
 
-def metade(preco=0):
-    return preco / 2
+def metade(preco=0, f=False):
+    res = preco / 2
+    if f:
+        return moeda(res)
+    else:
+        return f"{res:.2f}"
 
 
-def moeda(preco=0, m='R$'):
-    return f"{m} {preco:.2f}".replace('.', ',')
+def moeda(preco=0, f=False, m='R$'):
+    return f"{m}{preco:>8.2f}".replace('.', ',')
+
+
+def resumo(p, a, d):
+    print("".center(30, '-'))
+    print(f"RESUMO DE VALOR".center(30, ' '))
+    print("".center(30, '-'))
+    print(f"Preço analisado: {moeda(p)}")
+    print(f"Dobro do preço: {dobro(p, True)}")
+    print(f"Metado do preço: {metade(p, True)}")
+    print(f"{a}% de aumento: {aumentar(p, a, True)}")
+    print(f"{d}% de redução: {diminuir(p, d, True)}")
+    print("".center(30, '-'))
