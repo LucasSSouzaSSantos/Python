7836ab55b415cdd35f24e1efb17cb288d4a7d347
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-14 23:33:14 -0300

    Add files via upload

--- Modulo/moedas.py
+++ Modulo/moedas.py
@@ -1,15 +1,18 @@
-def aumentar(n, p):
-    return n + (n * p) / 100
+def aumentar(preco=0, taxa=0):
+    return preco + (preco * taxa) / 100
 
 
-def diminuir(n, p):
-    return n - (n * p) / 100
+def diminuir(preco=0, taxa=0):
+    return preco - (preco * taxa) / 100
 
 
-def dobro(p):
-    return 2 * p
+def dobro(preco=0):
+    return 2 * preco
 
 
-def metade(p):
-    return p / 2
-    
+def metade(preco=0):
+    return preco / 2
+
+
+def moeda(preco=0, m='R$'):
+    return f"{m} {preco:.2f}".replace('.', ',')
