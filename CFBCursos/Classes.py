07b7ea76b3fdf3199c15f3c2c39c8057fb28dc73
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-16 00:49:59 +0000

    Update Classes.py

--- CFBCursos/Classes.py
+++ CFBCursos/Classes.py
@@ -1,1 +1,18 @@
+# Criação de uma Classe:
+
+class Carro:
+    velMax = 0
+    ligado = False
+    cor = ""
+
+
+c1 = Carro()
+c2 = Carro()
+c3 = Carro()
+
+c1.velMax = 200
+c1.cor = "Preto"
+c1.ligado = False
+
+print(f"Velocidade Máxima: {c1.velMax}\nCor do Carro: {c1.cor}\nO Carro está ligado: {c1.ligado}")
 
