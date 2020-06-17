2b480d327404045a0eafb63b73e5f26052236b69
Lucas <lucsousan@gmail.com>
2020-06-17 19:28:12 -0300

    Update Tratamento_de_erro.py

--- CFBCursos/Tratamento_de_erro.py
+++ CFBCursos/Tratamento_de_erro.py
@@ -1,7 +1,14 @@
+x = 10
 try:
     # verifica se um bloco tem um erro
-except
+    print(x)
+except:
     # caso tenha ele execulta o except
-
-finally 
-    # caso contrário ele execulta esse bloco
+    print ("X não definido")
+else:
+    # caso não o código não tenha erro ele cai nesse bloco
+    print("Tudo certo!")
+finally:
+    # Independente se deu erro ou não, o bloco finally
+    # será execultado.
+    print("Fim do tratamento")
