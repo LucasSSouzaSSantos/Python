2e741ba994ea5807f6fd2ecf6a0a82f30643455c
Lucas <lucsousan@gmail.com>
2020-06-17 13:26:48 -0300

    Update Herança.py

--- CFBCursos/Herança.py
+++ CFBCursos/Herança.py
@@ -1,5 +1,21 @@
 # Herança
+# O que é herança ?
+class NPD: # Base, Pai, Super classe
+    def __init__(self, nome, time, forca, munnicao)
+    self.nome = nome 
+    self.time = time 
+    self.forca = forca
+    self.municao = municao
+    self.vivo = True
+    self.energia = 100
 
-
+    def info(self):
+        print("Nome:        "  + self.nome)
+        print("Time:        "  + str(self.time))
+        print("Força:       " + self.forca)
+        print("munição:     " + self.municao)
+        print("Vivo:        " + "Sim" if self.vivo else "Não")
+        print("Energia:     " + str(self.energia))
+        print("---------------------------------------")
 
 
