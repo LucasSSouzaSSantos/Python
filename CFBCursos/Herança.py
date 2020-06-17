8229a44e40883b4fe2c9344bad33ebe6fbba49eb
Lucas <lucsousan@gmail.com>
2020-06-17 14:45:06 -0300

    Update Herança.py

--- CFBCursos/Herança.py
+++ CFBCursos/Herança.py
@@ -1,46 +1,46 @@
-# Herança
-# O que é herança ?
-class NPC: # Base, Pai, Super classe
-    def __init__(self, nome, time, forca, munnicao)
-    self.nome = nome 
-    self.time = time 
-    self.forca = forca
-    self.municao = municao
-    self.vivo = True
-    self.energia = 100
+class NPC:
+    def __init__(self, nome, time, forca, municao):
+        self.nome = nome 
+        self.time = time 
+        self.forca = forca
+        self.municao = municao
+        self.vivo = True
+        self.energia = 100
 
     def info(self):
-        print("Nome:        "  + self.nome)
-        print("Time:        "  + str(self.time))
-        print("Força:       " + self.forca)
-        print("munição:     " + self.municao)
-        print("Vivo:        " + "Sim" if self.vivo else "Não")
-        print("Energia:     " + str(self.energia))
-        print("---------------------------------------")
+        print("Nome:...." + self.nome)
+        print("Time:...." + str(self.time))
+        print("Força:..." + str(self.forca))
+        print("munição:." + str(self.municao))
+        print("Vivo:...." + ("Sim" if self.vivo else "Não"))
+        print("Energia:." + str(self.energia))
 
 
-    class soldado(NPC):
-        def __init__(self, nome, time):
-            self.forca = 200
-            self.municao = 200
-            super().__init__(nome, time,self.forca, self.municao)
+class Soldado(NPC):
+    def __init__(self, nome, time):
+        self.forca = 200
+        self.municao = 200
+        super(Soldado, self).__init__(nome, time, self.forca, self.municao)
 
-    
-    class Guarda(NPC):
-        def __init__(self, nome, time):
-            self.forca = 100
-            self.municao = 20
-            super().__init__(nome, time, self.forca, self.municao)
-        def inf(self):
-            super().info()
-    
-    class Elite(NPC):
-        def __init__(self, nome, time):    
-            self.forca = 300
-            self.municao = 500
-            super().__init(nome, time, self.forca, self.municao)
-        def inf(self):
-            super().info()
+    def inf(self):
+        super(Soldado, self).info()
 
 
+class Guarda(NPC):
+    def __init__(self, nome, time):
+        self.forca = 100
+        self.municao = 20
+        super(Guarda, self).__init__(nome, time, self.forca, self.municao)
 
+    def inf(self):
+        super(Guarda, self).info()
+
+
+class Elite(NPC):
+    def __init__(self, nome, time):
+        self.forca = 400
+        self.municao = 500
+        super(Elite, self).__init__(nome, time, self.forca, self.municao)
+
+    def inf(self):
+        super(Elite, self).info()
