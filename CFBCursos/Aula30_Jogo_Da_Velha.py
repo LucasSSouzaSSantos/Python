c450f52d827840bbeb2fb99b73fc79b6c5a6264b
Lucas <lucsousan@gmail.com>
2020-06-17 22:41:25 -0300

    Update Aula30_Jogo_Da_Velha.py

--- CFBCursos/Aula30_Jogo_Da_Velha.py
+++ CFBCursos/Aula30_Jogo_Da_Velha.py
@@ -12,22 +12,3 @@ velha = [[" ", " ", " "],
          [" ", " ", " "]]
 
 
-def tela():
-    global velha
-    global jogadas
-    os.system("clear")
-    print("    0   1   2")
-    print("0:  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2])
-    print("   -----------")
-    print("0:  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[2][2])
-    print("   -----------")
-    print("0:  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2])
-    print("Jogadas: " + Fore.GREEN + str(jogadas) + Fore.RESET)
-
-while True:
-    tela()
-    #jog
-    #cpu
-    #vv
-    #
-
