ad994b5f0d3ee9770b26b6e22d852a711ec53f26
Lucas <lucsousan@gmail.com>
2020-06-18 14:09:46 -0300

    Update Aula35_Data.py

--- CFBCursos/Aula35_Data.py
+++ CFBCursos/Aula35_Data.py
@@ -4,3 +4,12 @@ data = datetime.datetime.now()
 
 print(str(data.day) + "/" + str(data.month) + "/" + str(data.year))
 
+nasc = datetime.datetime(1992,2, 21)
+
+print(nasc.strftime("%A"))
+
+# %a dia da semana resulmido
+# %A dia da semana completo
+# %w número do dia da semana domingo - 0, segunda - 1
+# %d númedo do dia do mês
+# 
