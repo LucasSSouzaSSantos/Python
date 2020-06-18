a88a00ea8a70343333ac82c613dff10ec9d41b5d
Lucas <lucsousan@gmail.com>
2020-06-18 14:13:50 -0300

    Update Aula35_Data.py

--- CFBCursos/Aula35_Data.py
+++ CFBCursos/Aula35_Data.py
@@ -12,4 +12,11 @@ print(nasc.strftime("%A"))
 # %A dia da semana completo
 # %w número do dia da semana domingo - 0, segunda - 1
 # %d númedo do dia do mês
-# 
+# %b nome do mês abreviado
+# %B nome do mês completo
+# %n número do mês
+# %y ano com dois dígitos
+# %Y ano com quatro dígitos
+# %H hora de 00 à 23
+# %I hora de 00 à 12
+ 
