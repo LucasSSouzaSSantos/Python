b557a0e29082bdf68cae9e749565b58b865d1ea4
Lucas <lucsousan@gmail.com>
2020-06-18 15:58:30 -0300

    Update Aula37.py

--- CFBCursos/Aula37.py
+++ CFBCursos/Aula37.py
@@ -2,7 +2,7 @@ import json
 
 carros_dicionario = {"marca": "honda", "modelo": "HRV", "cor": "prata"}
 # dicionário -> objeto json
-carros_d = json.dumps(carros_dicionario)
+carros_d = json.dumps(carros_dicionario, separators=(": ", " = "))
 print("Dicionário Python para Objeto JSON: \n", carros_d)
 
 carros_lista = ["honda", "volkswagem", "ford", "fiat", "chevrolet"]
@@ -14,3 +14,14 @@ carros_tupla = ("honda", "volkswagem", "ford", "fiat", "chevrolet")
 # tupla -> array json
 carros_t = json.dumps(carros_tupla)
 print("Tupla Python em Objeto JSON: \n", carros_t)
+
+# pode mudar o separador no caso trocar os dois pontos por um igual
+# usando o parâmetro separator
+
+# Arquivo Json
+
+arquivo_json = '{"nome": "Bruno","time":"aviadores","vivo": "True",' \
+               '"energia": 100,"mochila": ["perdeneira", "corda", ' \
+               '"linha", "arame"],"aeronaves": [{"tipo": "transporte"' \
+               ', "habilidade": 80},{"tipo": "ataque","habilidade": 100},' \
+               '{"tipo": "reconhecimento", "habilidade": 50}]} '
