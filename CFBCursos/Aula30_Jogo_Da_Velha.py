344a86490db6e781a5bbf0ff916162aef819e534
Lucas <lucsousan@gmail.com>
2020-06-17 22:25:04 -0300

    Update Aula30_Jogo_Da_Velha.py

--- CFBCursos/Aula30_Jogo_Da_Velha.py
+++ CFBCursos/Aula30_Jogo_Da_Velha.py
@@ -1,3 +1,14 @@
 import os
 import random
 from colorama import Fore, Back, Style
+
+jogarNovamente = "s"
+jogadas = 0
+quemJoga = 2
+maxJogadas = 9
+vit = "n"
+velha = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+
+
