385e4475f5ddabb62b0856f23685b4245bb610b1
Lucas <lucsousan@gmail.com>
2020-06-17 22:07:25 -0300

    Update Aula29_iterators.py

---
+++ CFBCursos/Aula29_iterators.py
@@ -0,0 +1,9 @@
+carros = ["HRV", "Polo", "Jetta", "Cruze", "Fusion"]
+
+itCarros = iter(carros)
+
+print(next(itCarros))
+print(next(itCarros))
+print(next(itCarros))
+print(next(itCarros))
+print(next(itCarros))
