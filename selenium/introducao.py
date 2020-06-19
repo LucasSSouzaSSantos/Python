5ba17387d313646b3d20248a68cb1ef34cbc28a6
Lucas <lucsousan@gmail.com>
2020-06-18 21:22:21 -0300

    Update introducao.py

--- selenium/introducao.py
+++ selenium/introducao.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Chrome(executable_path='./chromedriver')
+# Na linha de cima uma instância do chromedriver é criado e é passado
+# o caminho aonde o chromedrive estar, usando o comando executable_path
+driver.get("https://www.google.com/")
+# o método driver.get navegará para uma página fornecida pelo url
+assert "Google" in driver.title
+# A próxima linha é uma afirmação para confirmar que o título tem
+# a palavra "Google"
+elem = driver.find_element_by_name("q")
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_class_name("q")
 elem.clear()
-elem.send_keys("pycon")
-elem.send_send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
 
+elem.send_keys("bolsa de valores")
+
+elem.send_keys(Keys.RETURN)
+
+assert "No results found." not in driver.page_source
+
+driver.close()
