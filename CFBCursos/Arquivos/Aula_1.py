d11c35bfc42e039815578a67b24e261cabdb48c9
Lucas <lucsousan@gmail.com>
2020-06-21 15:06:25 -0300

    Update Aula_1.py

---
+++ CFBCursos/Arquivos/Aula_1.py
@@ -0,0 +1,13 @@
+f = open("teste.txt", "wt")
+"""
+r - read - Leitura
+a - append - anexar
+w - write   - Escrita
+x - create  - criar
+t - text - texto
+b - binary  - binario
+"""
+f.write("CFB Cursos")
+f.close()
+
+
