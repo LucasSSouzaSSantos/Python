2cffff70ac9917c8d015be09c15beab6f9f37136
Lucas <lucsousan@gmail.com>
2020-06-21 14:50:07 -0300

    Update Aula_3.py

--- CFBCursos/Tkinter/Aula_3.py
+++ CFBCursos/Tkinter/Aula_3.py
@@ -1,6 +1,20 @@
 from tkinter import *
+import os
 
-def impDados()
+
+c = os.path.dirname(__file__)
+nomeArquivo=c+"\\nomes.txt"
+
+
+def impDados():  # para quando apertar o botao
+    arquivo = open(nomeArquivo, "a")
+    arquivo.write(f"Nome: {vnome.get()}")
+    arquivo.write(f"\nTelefone: {vtelefone.get()}")
+    arquivo.write(f"\nEmail: {vemail.get()}")
+    arquivo.write(f"\nObs: {vobs.get(1.0, END)}")
+    arquivo.write("\n\n")
+    print("??????")
+
 
 app = Tk()
 app.title("CFB Cursos")
@@ -28,7 +42,10 @@ vemail.place(x=10, y=135, width=100, height=20)
 txt4 = Label(app, text="Fale Conosco", background="#dde", foreground="#000", anchor=W)
 txt4.place(x=10, y=160, width=100, height=20)
 
-vfaleConosco = Text(app, background="#fff", foreground="#000")
-vfaleConosco.place(x=10, y=185, width=150, height=50)
+vobs = Text(app, background="#fff", foreground="#000")
+vobs.place(x=10, y=185, width=150, height=50)
+
+btn1 = Button(app, text="Imprimir", command=impDados)
+btn1.place(x=10, y=240, width=100, height=20)
 
 app.mainloop()
