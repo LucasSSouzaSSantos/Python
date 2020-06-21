3725ef1d36d5fa636e92323cd4973eaee452aa80
Lucas <lucsousan@gmail.com>
2020-06-21 16:27:02 -0300

    Update instagram.py

--- selenium/instagram.py
+++ selenium/instagram.py
@@ -4,12 +4,12 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.get("https://www.instagram.com/")
 
-nome = driver.find_element_by_name("username", Keys.ARROW_DOWN)
+nome = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/span', Keys.ARROW_DOWN)
 nome.send_keys("#")
 
-senha = driver.find_element_by_name("password", Keys.ARROW_DOWN)
+senha = driver.find_element_by_xpath()('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/span//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/span', Keys.ARROW_DOWN)
 senha.send_keys("#")
 
 
-conectar = driver.find_element_by_xpath("")
+conectar = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button')
 conectar.click()
