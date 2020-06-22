b2ef2a6c7fd825ee077ed3ccbe14c92c15797c7e
Lucas <lucsousan@gmail.com>
2020-06-21 22:13:01 -0300

    Update Aula_2.py

--- CFBCursos/Banco_De_Dados/Aula_2.py
+++ CFBCursos/Banco_De_Dados/Aula_2.py
@@ -14,11 +14,22 @@ def ConexaoBanco():
 
 vcon = ConexaoBanco()
 
-def inserir(conexao, sql):
+vsql = """CREATE TABLE tb_contatos(
+    N_IDCONTATO INTEGER PRIMARY KEY AUTOINCREMENT,
+    T_NOMECONTATO VARCHAR(30),
+    T_TELEFONECONTATO VARCHAR(14),
+    T_EMAILCONTATO VARCHAR(30)
+);"""
+def criarTabela(conexao, sql):
     try:
         c = conexao.cursor()
-        c = execute(sql)
-        # conexao.commit()
-    except Erro as ex:
+        c.execute(sql)
+        print("Tabela criada")
+    except Error as ex:
         print(ex)
+
+criarTabela(vcon, vsql)
+
+vcon.close()
+
 
