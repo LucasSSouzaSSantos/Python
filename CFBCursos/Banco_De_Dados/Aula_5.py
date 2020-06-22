995a165d72b6f0ede020a4a99559414f5e32ab11
Lucas <lucsousan@gmail.com>
2020-06-22 12:03:02 -0300

    Update Aula_5.py

---
+++ CFBCursos/Banco_De_Dados/Aula_5.py
@@ -0,0 +1,23 @@
+import sqlite3
+from sqlite3 import Error
+
+# Criar Conexão com  o banco de dados usando a função
+################ Criar Conexão ######################
+def ConexaoBanco():
+    caminho = "//home//lucas//Documentos//GitHub//Python//CFBCursos//Banco_De_Dados//Agenda.db"
+    con=None
+    try:
+        con=sqlite3.connect(caminho)
+        print("Conexão Com Sucesso!")
+    except Error as ex:
+        print(ex)
+    return con
+
+vcon = ConexaoBanco()
+
+
+
+
+
+
+
