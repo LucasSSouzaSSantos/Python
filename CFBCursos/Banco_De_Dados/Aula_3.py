5feafc4875f788e736f992227102d07a8328fe78
Lucas <lucsousan@gmail.com>
2020-06-21 22:13:09 -0300

    Update Aula_3.py

--- CFBCursos/Banco_De_Dados/Aula_3.py
+++ CFBCursos/Banco_De_Dados/Aula_3.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 from sqlite3 import Error
 
@@ -14,20 +15,10 @@ def ConexaoBanco():
 
 vcon = ConexaoBanco()
 
-vsql = """CREATE TABLE tb_contatos(
-    N_IDCONTATO INTEGER PRIMARY KEY AUTOINCREMENT,
-    T_NOMECONTATO VARCHAR(30),
-    T_TELEFONECONTATO VARCHAR(14),
-    T_EMAILCONTATO VARCHAR(30)
-);"""
-def criarTabela(conexao, sql):
+def inserir(conexao, sql):
     try:
         c = conexao.cursor()
-        c.execute(sql)
-        print("Tabela criada")
-    except Error as ex:
+        c = execute(sql)
+        # conexao.commit()
+    except Erro as ex:
         print(ex)
-
-criarTabela(vcon, vsql)
-
-vcon.close()
