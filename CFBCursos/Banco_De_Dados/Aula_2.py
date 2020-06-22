09c1684f5fc3e9dbd047c5954b50b3660cdd8572
Lucas <lucsousan@gmail.com>
2020-06-21 21:49:52 -0300

    Update Aula_2.py

--- CFBCursos/Banco_De_Dados/Aula_2.py
+++ CFBCursos/Banco_De_Dados/Aula_2.py
@@ -1,6 +1,33 @@
 import sqlite3
 from sqlite3 import Error
 
-# Criar Conexão com  o banco de dados
+# Criar Conexão com  o banco de dados usando a função
+################ Criar Conexão ######################
+def ConexaoBanco():
+    caminho = "//home//lucas//Documentos//GitHub//Python//CFBCursos//Banco_De_Dados//Agenda.db"
+    con=None
+    try:
+        con=sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    return con
 
+vcon = ConexaoBanco()
 
+vsql = """CREATE TABLE tb_contatos3(
+    N_IDCONTATO INTEGER PRIMARY KEY AUTOINCREMENT,
+    T_NOMECONTATO VARCHAR(30),
+    T_TELEFONECONTATO VARCHAR(14),
+    T_EMAILCONTATO VARCHAR(30)
+);"""
+def criarTabela(conexao, sql):
+    try:
+        c = conexao.cursor()
+        c.execute(sql)
+        print("Tabela criada")
+    except Error as ex:
+        print(ex)
+
+criarTabela(vcon, vsql)
+
+vcon.close()
