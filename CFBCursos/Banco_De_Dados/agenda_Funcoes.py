8494a9af7a0b286744c9dad1841f0396863fcf7d
Lucas <lucsousan@gmail.com>
2020-06-23 12:02:44 -0300

    Update agenda_Funcoes.py

--- CFBCursos/Banco_De_Dados/agenda_Funcoes.py
+++ CFBCursos/Banco_De_Dados/agenda_Funcoes.py
@@ -1,7 +1,7 @@
 def menuPrincipal():
     print("1 - Inserir Novo Registro ")
     print("2 - Deletar Registro ")
-    print("Atualizar Registro ")
+    print("3 - Atualizar Registro ")
     print("4 - Consultar Registro por ID ")
     print("5 - Consultar Resgistro por Nome ")
     print("6 - Sair")
