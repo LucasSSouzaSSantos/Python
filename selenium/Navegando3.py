ec5a00a3c8a199e14dbdfbecfc0aa3575433924f
Lucas <lucsousan@gmail.com>
2020-06-22 21:34:52 -0300

    Update Navegando3.py

--- selenium/Navegando3.py
+++ selenium/Navegando3.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-
+import time
 driver = webdriver.Chrome(executable_path='./chromedriver')
-driver.get()
+driver.get("file:///home/lucas/Documentos/GitHub/Python/selenium/teste.html")
 """
 Como a maneira vista no arquivo Navengando2.py não é melhor maneira de navegar entre os select,
 vamos tentar um nova abordagem. As classes de suporte do webDriver incluem uma chamada "select",
 que fornece métodos úteis para interagir com estes(select):
 
 """
+
+time.sleep(3)
+driver.close()
