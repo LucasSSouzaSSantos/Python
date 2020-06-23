ad07797606f0e3946fa94e11b61e4eec111dde39
Lucas <lucsousan@gmail.com>
2020-06-23 12:20:55 -0300

    Update agenda_Funcoes.py

--- CFBCursos/Banco_De_Dados/agenda_Funcoes.py
+++ CFBCursos/Banco_De_Dados/agenda_Funcoes.py
@@ -1,3 +1,20 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def ConexaoBanco():
+    caminho = "//home//lucas//Documentos//GitHub//Python//CFBCursos//Banco_De_Dados//Agenda.db"
+    con = None
+    try:
+        con = sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    else:
+        print("Conexao realizada com sucesso! ")
+    return con
+
+
+
 def menuPrincipal():
     print("1 - Inserir Novo Registro ")
     print("2 - Deletar Registro ")
@@ -5,3 +22,23 @@ def menuPrincipal():
     print("4 - Consultar Registro por ID ")
     print("5 - Consultar Resgistro por Nome ")
     print("6 - Sair")
+
+
+def menuAtualizar():
+    print()
+
+
+def menuConsultarId():
+    print()
+
+
+def menuConsultarNomes():
+    print()
+
+
+def menuDeletar():
+    print()
+
+
+def menuInserir():
+    print()
