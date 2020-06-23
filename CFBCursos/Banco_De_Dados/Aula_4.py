a8c5795836cc1cb607d6fc031727b8268db731cd
Lucas <lucsousan@gmail.com>
2020-06-23 19:00:03 -0300

    Update Aula_4.py

--- CFBCursos/Banco_De_Dados/Aula_4.py
+++ CFBCursos/Banco_De_Dados/Aula_4.py
@@ -1,10 +1,7 @@
-# Deletando registro do banco de dados
-
 import sqlite3
 from sqlite3 import Error
 
-# Criar Conexão com  o banco de dados usando a função
-################ Criar Conexão ######################
+
 def ConexaoBanco():
     caminho = "//home//lucas//Documentos//GitHub//Python//CFBCursos//Banco_De_Dados//Agenda.db"
     con=None
@@ -15,10 +12,11 @@ def ConexaoBanco():
         print(ex)
     return con
 
+
 vcon = ConexaoBanco()
 
+
 def deletar(conexao, sql):
-   # num = input("Digite um número: ")
     try:
         c = conexao.cursor()
         c.execute(sql)
@@ -26,6 +24,7 @@ def deletar(conexao, sql):
         print("Dados deletado com sucesso!")
     except Error as ex:
         print(ex)
+
 
 vsql = "DELETE FROM tb_contatos WHERE n_IdContatos=1"
 deletar(vcon, vsql)
