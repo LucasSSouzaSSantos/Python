0ab4f103972abdf9a537e69ead423e8390470dbb
Lucas <lucsousan@gmail.com>
2020-06-22 21:33:23 -0300

    Update Navegando2.py

--- selenium/Navegando2.py
+++ selenium/Navegando2.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path='./chromedriver')
-driver.get("file:///home/lucas/Documentos/GitHub/Python/selenium/teste.html")
+driver.get("url da página")
 
 element = driver.find_element_by_xpath("//select[@name='carros']")
 all_options = element.find_elements_by_tag_name("option")
