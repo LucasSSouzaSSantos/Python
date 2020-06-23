73b58f8b61ecc455e583f8b5f5e7e039d4bbbefa
Lucas <lucsousan@gmail.com>
2020-06-23 12:20:51 -0300

    Update agenda.py

--- CFBCursos/Banco_De_Dados/agenda.py
+++ CFBCursos/Banco_De_Dados/agenda.py
@@ -1,37 +1,21 @@
-import sqlite3
-from sqlite3 import Error
-from CFBCursos.Banco_De_Dados.agenda_Funcoes import menuPrincipal
-# conexao
-
-
-def ConexaoBanco():
-    caminho = "//home//lucas//Documentos//GitHub//Python//CFBCursos//Banco_De_Dados//Agenda.db"
-    con = None
-    try:
-        con = sqlite3.connect(caminho)
-    except Error as ex:
-        print(ex)
-    else:
-        print("Conexao realizada com sucesso! ")
-    return con
-
+from CFBCursos.Banco_De_Dados.agenda_Funcoes import menuPrincipal, menuAtualizar, menuInserir, ConexaoBanco
+from CFBCursos.Banco_De_Dados.agenda_Funcoes import menuConsultarId, menuConsultarNomes, menuDeletar
 
 vcon = ConexaoBanco()
 opc = 0
-
 while True:
     menuPrincipal()
     opc = int(input("Digite uma opção: "))
     if opc == 1:
-        print("opc 1")
+        menuInserir()
     elif opc == 2:
-        print("opc 2")
+        menuDeletar()
     elif opc == 3:
-        print("opc 3")
+        menuAtualizar()
     elif opc == 4:
-        print("opc 4")
+        menuConsultarId()
     elif opc == 5:
-        print("opc 5")
+        menuConsultarNomes()
     elif opc == 6:
         print("Programa finalizado!")
         break
