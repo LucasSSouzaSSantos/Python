164db184846f81598658114596c4b81d577a87a9
Lucas <lucsousan@gmail.com>
2020-06-23 19:00:09 -0300

    Update Aula_5.py

--- CFBCursos/Banco_De_Dados/Aula_5.py
+++ CFBCursos/Banco_De_Dados/Aula_5.py
@@ -1,36 +1,27 @@
 import sqlite3
 from sqlite3 import Error
 
-# Criar Conexão com  o banco de dados usando a função
-################ Criar Conexão ######################
+
 def ConexaoBanco():
     caminho = "//home//lucas//Documentos//GitHub//Python//CFBCursos//Banco_De_Dados//Agenda.db"
-    con=None
+    con = None
     try:
-        con=sqlite3.connect(caminho)
+        con = sqlite3.connect(caminho)
         print("Conexão Com Sucesso!")
     except Error as ex:
         print(ex)
     return con
 
+
 vcon = ConexaoBanco()
 
-# nome = input("Digite o nome: ")
-# telefone = input("Digite o telefone: ")
-# email = input("Digite o email: ")
 
-# vsql = "insert into tb_contatos (T_NomeContato, T_TelefoneContato, T_EmailContato) values ('"+nome+"','"+telefone+"','"+email+"')"
+def consultar(conexao, sql):
+    c = conexao.cursor()
+    c.execute(sql)
+    res = c.fetchall()
+    print(res)
 
-# def inserir(conexao, sql):
-#    try:
-#        c = conexao.cursor()
-#        c.execute(sql)
-#        print("Dados inseridos com sucesso!")
-#        conexao.commit()
-#    except Error as ex:
-#        print(ex)
-
-# inserir(vcon, vsql)
 
 def atualizar(conexao, sql):
     try:
@@ -42,7 +33,6 @@ def atualizar(conexao, sql):
     else:
         print("Registro finalizado")
 
-vsql = "UPDATE tb_contatos set t_NomeContato='Marcos' where n_IdContatos=2"
-atualizar(vcon, vsql)
 
-
+vsql = "SELECT * FROM tb_contatos WHERE n_IdContatos = 1"
+consultar(vcon, vsql)
