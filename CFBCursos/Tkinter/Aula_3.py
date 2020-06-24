e7ee1321f3e19f98d730b6aa8c9153076d9a0d29
Lucas <lucsousan@gmail.com>
2020-06-24 14:08:00 -0300

    Update Aula_3.py

--- CFBCursos/Tkinter/Aula_3.py
+++ CFBCursos/Tkinter/Aula_3.py
@@ -1,19 +1,27 @@
 from tkinter import *
 import os
-
+from CFBCursos.Tkinter.banco import dml, dql
 
 c = os.path.dirname(__file__)
-nomeArquivo=c+"\\nomes.txt"
+nomeArquivo = c+"\\nomes.txt"
 
 
-def impDados():  # para quando apertar o botao
-    arquivo = open(nomeArquivo, "a")
-    arquivo.write(f"Nome: {vnome.get()}")
-    arquivo.write(f"\nTelefone: {vtelefone.get()}")
-    arquivo.write(f"\nEmail: {vemail.get()}")
-    arquivo.write(f"\nObs: {vobs.get(1.0, END)}")
-    arquivo.write("\n\n")
-    print("??????")
+def gravarDados():  # para quando apertar o botao
+    if tb_nome.get() != "":
+        vnome = tb_nome.get()
+        vfone = tb_fone.get()
+        vemail = tb_email.get()
+        vobs = tb_obs.get("1.0", END)
+        vquery = "INSERT INTO tb_contatos (t_NOMECONTATO, t_TELEFONECONTATO, t_EMAILCONTATO, t_OBS)" \
+                 "VALUES ('"+vnome+"', '"+vfone+"', '"+vemail+"', '"+vobs+"')"
+        dml(vquery)
+        tb_nome.delete(0, END)
+        tb_fone.delete(0, END)
+        tb_email.delete(0, END)
+        tb_obs.delete("1.0", END)
+        print("Dados Gravados")
+    else:
+        print("Erro!")
 
 
 app = Tk()
@@ -24,28 +32,28 @@ app.configure(background="#dde")
 txt1 = Label(app, text="Nome", background="#dde", foreground="#000", anchor=W)
 txt1.place(x=10, y=10, width=100, height=20)
 
-vnome = Entry(app, background="#fff", foreground="#000")
-vnome.place(x=10, y=35, width=100, height=20)
+tb_nome = Entry(app, background="#fff", foreground="#000")
+tb_nome.place(x=10, y=35, width=100, height=20)
 
 txt2 = Label(app, text="Telefone", background="#dde", foreground="#000", anchor=W)
 txt2.place(x=10, y=60, width=100, height=20)
 
-vtelefone = Entry(app, background="#fff", foreground="#000")
-vtelefone.place(x=10, y=85, width=100, height=20)
+tb_fone = Entry(app, background="#fff", foreground="#000")
+tb_fone.place(x=10, y=85, width=100, height=20)
 
 txt3 = Label(app, text="E-mail", background="#dde", foreground="#000", anchor=W)
 txt3.place(x=10, y=110, width=100, height=20)
 
-vemail = Entry(app, background="#fff", foreground="#000")
-vemail.place(x=10, y=135, width=100, height=20)
+tb_email = Entry(app, background="#fff", foreground="#000")
+tb_email.place(x=10, y=135, width=100, height=20)
 
 txt4 = Label(app, text="Fale Conosco", background="#dde", foreground="#000", anchor=W)
 txt4.place(x=10, y=160, width=100, height=20)
 
-vobs = Text(app, background="#fff", foreground="#000")
-vobs.place(x=10, y=185, width=150, height=50)
+tb_obs = Text(app, background="#fff", foreground="#000")
+tb_obs.place(x=10, y=185, width=150, height=50)
 
-btn1 = Button(app, text="Imprimir", command=impDados)
+btn1 = Button(app, text="Imprimir", command=gravarDados)
 btn1.place(x=10, y=240, width=100, height=20)
 
 app.mainloop()
