c5d4e08674ef83084484209eb7c28985a515f02f
Lucas <lucsousan@gmail.com>
2020-06-24 13:08:24 -0300

    Create agenda.py

---
+++ CFBCursos/Tkinter/agenda.py
@@ -0,0 +1,25 @@
+from CFBCursos.Tkinter.agenda_Funcoes import menuPrincipal, menuAtualizar, menuInserir
+from CFBCursos.Tkinter.agenda_Funcoes import menuConsultar, menuConsultarNomes, menuDeletar
+
+
+while True:
+    print(15 * "-=-")
+    menuPrincipal()
+    print(15 * "-=-")
+    opc = int(input("Digite uma opção: "))
+    if opc == 1:
+        menuInserir()
+    elif opc == 2:
+        menuDeletar()
+    elif opc == 3:
+        menuAtualizar()
+    elif opc == 4:
+        menuConsultar()
+    elif opc == 5:
+        menuConsultarNomes()
+    elif opc == 6:
+        print("Programa finalizado!")
+        break
+    else:
+        print("Opção Invalida")
+    print(15 * "-=-")
