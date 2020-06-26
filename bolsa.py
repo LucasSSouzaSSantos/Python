99558e3d3b10771bcc514460f35da64b812bd54f
Lucas <lucsousan@gmail.com>
2020-06-26 19:25:23 -0300

    Update bolsa.py

--- bolsa.py
+++ bolsa.py
@@ -6,26 +6,10 @@ import plotly.graph_objects as go
 df = pd.DataFrame()
 
 # escolher a ação
-acao = 'ITUB3.SA'
+acao1 = 'ITUB3.SA'
+acao2 = 'Dow 30'
 
 # importar dados par o DataFrame
-df = web.DataReader(acao, data_source='yahoo', start='01-01-2000')
+df = web.DataReader(acao2, data_source='yahoo', start='01-01-2000')
 
 print(df.head())
-
-trace1 = {
-    'x': df.index,
-    'open': df.Open,
-    'close': df.Close,
-    'high': df.High,
-    'low': df.Low,
-    'type': 'candlestick',
-    'name': acao,
-    'showlegend': False
-}
-
-data = [trace1]
-layout = go.Layout()
-
-fig = go.Figure(data=data, layout=layout)
-fig.show()
