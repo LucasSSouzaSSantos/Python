9c15c6ea9c3f3d9dd17e65d1c12aec97a0390635
Lucas <lucsousan@gmail.com>
2020-06-26 22:52:55 -0300

    Update Aula_1.py

--- Pandas/Aula_1.py
+++ Pandas/Aula_1.py
@@ -17,7 +17,33 @@ Representação da tabela de dados do Pandas
 +-------+-------+-------+-------+-------+
                         |Column |
                         +-------+
+Eu quero armazenar dados de passageiros do Titanic. 
+Para vários passageiros, eu sei os dados de 
+nome (caracteres)
+idade (números inteiros)
+sexo (masculino / feminino)
+"""
 
+df = pd.DataFrame({
+    "Name": ["Braund, Mr. William Henry",
+             "Allen, Mr. William Henry",
+             "male, Miss. Elizabeth"],
+    "Age": [22, 35, 58],
+    "Sex": ["male", "male", "female"]}
+)
 
+print(df)
 
+"""
+Para armazenar dados manualmente em uma tabela, crie
+um DataFrame. Ao usar um dicionário de listas Python,
+as chaves do dicionário serão usadas como cabeçalhos 
+de coluna e os valores em cada lista como linhas da 
+DataFrame.
+                                                        
+A DataFrameé uma estrutura de dados bidimensional que 
+pode armazenar dados de diferentes tipos (incluindo 
+caracteres, números inteiros, valores de ponto flutuan
+te, dados categóricos e mais) em colunas. É semelhante
+a uma planilha, uma tabela SQL ou a data.frameem R.
 """
