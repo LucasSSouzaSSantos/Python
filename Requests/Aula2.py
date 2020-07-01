0c0b06b6cd714a253842cdaffbf2b1672e0611cd
Lucas <lucsousan@gmail.com>
2020-06-30 21:35:08 -0300

    Update Aula2.py

--- Requests/Aula2.py
+++ Requests/Aula2.py
@@ -1,5 +1,16 @@
+# Passando parametros em URLs
 import requests
 
+"""
+url = 'http://httpbin.org/get'
+payload = {'num1': 10, 'num2': 12}
+pagina = requests.get(url,params=payload)
+print pagina.url')
+"""
 url = 'http://httpbin.org/get'
 pagina = requests.get(url)
-print(f"{pagina}")
+# print(f"{pagina.text}")
+print(f"{pagina.encoding}")
+
+# Resposta binário
+print(f"{pagina.content}")
