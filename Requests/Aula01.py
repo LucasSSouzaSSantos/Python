4dedf294bcb053e06d88c0c65eecc69690ae8f02
Lucas <lucsousan@gmail.com>
2020-07-08 20:31:11 -0300

    Update Aula01.py

--- Requests/Aula01.py
+++ Requests/Aula01.py
@@ -1,29 +1,11 @@
 # Fazendo um requisição
 import requests
-
-url = '' 
-requisicao_get = requests.get(url)
-print(f"Requisição GET: {requisicao_get}")
-
+import json
+r = requests.get('http://httpbin.org/get', auth=('user', 'pass'))
+print(r.content)
 """
 import requests importação do módulo para fazer requisição
 url = domínio da página que foi passado como parâmetro
 pagina = instância de requests
 .get(url) = método para pegar as informações da url passada
 """
-
-requisicao_post = requests.post(url)
-print(f"Requisição POST: {requisicao_post}")
-
-requisicao_put = requests.put(url)
-print(f"Requisição PUT: {requisicao_put}")
-
-
-requisicao_delete = requests.delete(url)
-print(f"Requisição DELETE: {requisicao_delete}")
-
-requisicao_head = requests.head(url)
-print(f"Requisição HEAD: {requisicao_head}")
-
-requisicao_options = requests.options(url)
-print(f"Requisição OPTIONS: {requisicao_options}")
